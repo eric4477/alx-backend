c07af65630fac5ed1e525b179b58dbe231c80836
eric4477 <ericgeorge4477@gmail.com>
2024-07-20 16:41:33 +0300

    format

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -17,8 +17,7 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -28,8 +27,7 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
+        """Dataset indexed by sorting position, starting at 0"""
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             self.__indexed_dataset = {
@@ -37,7 +35,8 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict[str, Any]:
+    def get_hyper_index(self, index: int = None, 
+                        page_size: int = 10) -> Dict[str, Any]:
         """
         Get a page from the dataset with deletion-resilience.
 
@@ -56,7 +55,8 @@ class Server:
         next_index = index
 
         for _ in range(page_size):
-            while next_index not in indexed_dataset and next_index < len(self.dataset()):
+            while (next_index not in indexed_dataset and 
+                   next_index < len(self.dataset())):
                 next_index += 1
             if next_index >= len(self.dataset()):
                 break
