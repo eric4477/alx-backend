eca527af15aab51868704767893c37a335f8ee42
eric4477 <ericgeorge4477@gmail.com>
2024-07-25 01:52:49 +0300

    basic-cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+BasicCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache implements a simple caching system without limit"""
+
+    def put(self, key, item):
+        """Assign the item value for the key to self.cache_data"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Return the value in self.cache_data linked to key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
---
+++ 0x01-caching/base_caching.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+BaseCaching module
+"""
+
+
+class BaseCaching:
+    """BaseCaching defines a caching system"""
+
+    def __init__(self):
+        """Initialize the caching system"""
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """Add an item to the cache"""
+        raise NotImplementedError("put() must be implemented in subclasses")
+
+    def get(self, key):
+        """Get an item from the cache"""
+        raise NotImplementedError("get() must be implemented in subclasses")
