e7c0a7d6d6f9ae8d028cb48257db8c73188f0da8
eric4477 <ericgeorge4477@gmail.com>
2024-07-25 02:26:33 +0300

    refactor

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python3
+"""LIFOCache module
 """
-LIFOCache module
-"""
+from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """LIFOCache implements a LIFO caching system"""
-
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LIFO
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
-        """Initialize the LIFO caching system"""
+        """Initializes the cache.
+        """
         super().__init__()
-        self.keys_order = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Assign the item value for the key to self.cache_data"""
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                self.keys_order.remove(key)
-            self.cache_data[key] = item
-            self.keys_order.append(key)
-
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                last_key = self.keys_order.pop(-1)
-                del self.cache_data[last_key]
-                print(f"DISCARD: {last_key}")
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(last=True)
+                print("DISCARD:", last_key)
+        
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        """Return the value in self.cache_data linked to key"""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
