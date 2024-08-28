5cbab9ad6c6ff852a01419e4c91cba0677e85832
eric4477 <ericgeorge4477@gmail.com>
2024-08-29 00:18:27 +0300

    Complete 8-job.js and 8-jobs.test.js

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,20 @@
+// This function takes an array of job data and a Kue queue instance.
+// It creates a new job for each item in the array
+export default function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) throw new Error("Jobs is not an array");
+  jobs.forEach((jobData) => {
+    const job = queue.create("push_notification_code_3", jobData);
+    job.save((error) => {
+      if (!error) console.log(`Notification job created: ${job.id}`);
+    });
+    job.on("complete", () =>
+      console.log(`Notification job ${job.id} completed`)
+    );
+    job.on("progress", (progress) =>
+      console.log(`Notification job ${job.id} ${progress}% completed`)
+    );
+    job.on("failed", (errorMessage) =>
+      console.log(`Notification job ${job.id} failed: ${errorMessage}`)
+    );
+  });
+}
